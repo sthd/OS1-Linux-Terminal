cf3e7db8c7db5e6070cae92f71e679a05ddc5c1c
elior <sch@campus.technion.ac.il>
2021-11-22 23:09:42 +0200

    Initial Commit

---
+++ OS1draft/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OS1draft
+//
+//  Created by Elior on 22/11/2021.
+//  Copyright © 2021 Elior. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
